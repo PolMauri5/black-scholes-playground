431f3f976ecf19c4a964bf6f74573183ececd987
PolMauri5 <polmauri5@gmail.com>
2025-12-11 15:07:23 +0100

    feat: d1 + d2 functions implemented

--- src/bs/black_scholes.rs
+++ src/bs/black_scholes.rs
@@ -30,16 +30,38 @@ fn price_put_sub() -> f64 {
     0.0
 }
 
-/// d1 — also called “standardized moneyness”.
+/// d1 — also called “standardized moneyness, z-index...”.
 /// It measures how many standard deviations the log-price is above the strike
 /// once drift and volatility scaling are taken into account.
 /// This is the core input to all Black-Scholes Greeks.
+/// Its normaly a number between -3 / +3.
+/// If we are operating a call, a positive d1 is good for us.
 fn standardized_moneyness(
     option: &Option,
     underlying: &UnderlyingAsset,
     market: &MarketParams,
 ) -> f64 {
-    todo!()
+    let s = underlying.spot;
+    let k = option.strike;
+    let r = market.rate;
+    // IV: Implied volatility.
+    let sigma = market.volatility;
+    let t = option.time_to_expiry;
+
+    if sigma <= 0.0 || t <= 0.0 {
+        return 0.0;
+    }
+
+    // Logarithmic distance between spot ans strike.
+    let log_moneyness = (s / k).ln();
+
+    // How the expected log-price evolves over time
+    let carry_term = (r + 0.5 * sigma * sigma) * t;
+
+    // Standard deviation of the asset's log return over the time.
+    let denom = sigma * t.sqrt();
+
+    (log_moneyness + carry_term) / denom
 }
 
 /// d2 — also called “forward-adjusted standardized moneyness”.
@@ -50,5 +72,18 @@ fn standardized_moneyness_forward(
     underlying: &UnderlyingAsset,
     market: &MarketParams, 
 ) -> f64 {
-    todo!()
+    let sigma = market.volatility;
+    let t = option.time_to_expiry;
+
+    if sigma <= 0.0 || t <= 0.0 {
+        return 0.0;
+    }
+
+    let d1 = standardized_moneyness(option, underlying, market);
+
+    // This represents one "volatility shock" over the whol horizon.
+    let vol_shock = sigma * t.sqrt();
+
+    // D2 is simply d1 shifted down by one volatility shock
+    d1 - vol_shock
 }
